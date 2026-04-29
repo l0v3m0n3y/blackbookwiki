1439c474a6f3b3070d768b7e0677c623be8e9dea
in my mind... <94906343+l0v3m0n3y@users.noreply.github.com>
2026-04-29 17:40:12 +0300

    Create blackbookwiki.swift

---
+++ src/blackbookwiki.swift
@@ -0,0 +1,72 @@
+import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
+
+extension URLSession {
+    func data(for request: URLRequest) async throws -> (Data, URLResponse) {
+        return try await withCheckedThrowingContinuation { continuation in
+            let task = self.dataTask(with: request) { data, response, error in
+                if let error = error {
+                    continuation.resume(throwing: error)
+                } else if let data = data, let response = response {
+                    continuation.resume(returning: (data, response))
+                } else {
+                    continuation.resume(throwing: URLError(.unknown))
+                }
+            }
+            task.resume()
+        }
+    }
+}
+
+public class Blackbookwiki {
+    private let api = "https://black-book.wiki/api/v1"
+    private var headers: [String: String]
+    
+    public init() {
+        self.headers = [
+        "Accept":"*/*",
+        "Connection":"keep-alive",
+        "Accept-Encoding":"deflate, zstd",
+        "Accept-Language":"en-US,en;q=0.9",
+        "Host":"black-book.wiki",
+        "User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36"
+        ]
+
+    }
+    
+    public func persons_random() async throws -> Any {
+        guard let url = URL(string: "\(api)/persons_random") else {
+            throw NSError(domain: "Invalid URL", code: -1)
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
+        let (data, _) = try await URLSession.shared.data(for: request)
+        return try JSONSerialization.jsonObject(with: data)
+    }
+    
+    public func persons_coordinates() async throws -> Any {
+        guard let url = URL(string: "\(api)/persons_coordinates/") else {
+            throw NSError(domain: "Invalid URL", code: -1)
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
+        let (data, _) = try await URLSession.shared.data(for: request)
+        return try JSONSerialization.jsonObject(with: data)
+    }
+    
+    public func get_persons_list(conf: String) async throws -> Any {
+        //confs: internet_blocking=true,case_navalny=true,poisoning_navalny=true
+        guard let url = URL(string: "\(api)/persons/?\(conf)") else {
+            throw NSError(domain: "Invalid URL", code: -1)
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
+        let (data, _) = try await URLSession.shared.data(for: request)
+        return try JSONSerialization.jsonObject(with: data)
+    }
+}
